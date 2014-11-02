833c18d092416590feb8969df39e04923528b8a2
Reactive Raven <reactiveraven@gmail.com>
2014-11-02 21:16:14 +0000

    Initial commit

---
+++ test/karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: "./../www/",
+
+    files: [
+      "lib/angular/angular.js",
+      "lib/angular-mocks/angular-mocks.js",
+      "lib/angular-animate/angular-animate.js",
+      "lib/angular-sanitize/angular-sanitize.min.js",
+      "lib/angular-ui-router/release/angular-ui-router.js",
+      "lib/angular-resource/angular-resource.min.js",
+      "lib/ng-modular-log/ngModularLog.js",
+      "lib/ionic/js/ionic.js",
+      "lib/ionic/js/ionic-angular.js",
+      "js/**/*.js"
+    ],
+
+    autoWatch: true,
+
+    frameworks: [ "jasmine" ],
+
+    browsers: [ "PhantomJS" ],
+
+    plugins: [
+      "karma-phantomjs-launcher",
+      "karma-jasmine"
+    ],
+
+    junitReporter: {
+      outputFile: "test_out/unit.xml",
+      suite: "unit"
+    }
+
+  });
+};
